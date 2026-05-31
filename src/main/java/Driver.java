7a882ea204184b602506338dbac07cdbc0326e24
Raihan <raihanwibyanto@gmail.com>
2026-05-31 22:29:21 +0700

    feat: update driver part4

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,16 +5,30 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
+		
+		System.out.println("Current Count:" + counter.getCount());
+		
+		counter.increaseBy(6);
 
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.getCount());
 		
-		counter.increment();
+		counter.decreaseBy(3);
+
+		System.out.println("Current Count:" + counter.getCount());
 		
-		System.out.println("Count:" + counter.getCount());
+		counter.multiplyBy(6);
+
+		System.out.println("Current Count:" + counter.getCount());
+
+		// System.out.println("Count:" + counter.getCount());
 		
-		counter.decrement();
+		// counter.increment();
 		
-		System.out.println("Count:" + counter.getCount());
+		// System.out.println("Count:" + counter.getCount());
+		
+		// counter.decrement();
+		
+		// System.out.println("Count:" + counter.getCount());
 		
 	}
 
